cb4df542309e7857d11b4c45626ce869a5a0e404
Fabio Ponciroli <ponch78@gmail.com>
2026-05-15 18:01:37 +0200

    Add acknowledgement-aware Kinesis consumer logic
    Add configurable manual acknowledgement support for Kinesis consumers
    through plugin.events-aws-kinesis.enableAutoCommit configuration.
    
    When automatic commit is disabled, consumers receive an acknowledgement
    handle that checkpoints the current record sequence number on
    ack(event).
    
    Automatic checkpointing behaviour remains unchanged when auto-commit is
    enabled.
    
    Update init prompts for the new acknowledgement mode.
    
    Bug: Issue 486806599
    Change-Id: I34446b10f592dfbc9215639576655ea1346c4d16

---
+++ src/main/java/com/gerritforge/gerrit/plugins/kinesis/KinesisRecordAcknowledgement.java
@@ -0,0 +1,53 @@
+// Copyright (C) 2026 GerritForge, Inc.
+//
+// Licensed under the BSL 1.1 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.plugins.kinesis;
+
+import com.gerritforge.gerrit.eventbroker.MessageAcknowledgement;
+import com.gerritforge.gerrit.eventbroker.MessageAcknowledgementException;
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.server.events.Event;
+import java.util.concurrent.atomic.AtomicBoolean;
+import software.amazon.kinesis.processor.RecordProcessorCheckpointer;
+import software.amazon.kinesis.retrieval.KinesisClientRecord;
+
+class KinesisRecordAcknowledgement implements MessageAcknowledgement<Event> {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  private final KinesisClientRecord consumerRecord;
+  private final RecordProcessorCheckpointer checkpointer;
+  private final AtomicBoolean acked = new AtomicBoolean(false);
+
+  KinesisRecordAcknowledgement(
+      KinesisClientRecord consumerRecord, RecordProcessorCheckpointer checkpointer) {
+    this.consumerRecord = consumerRecord;
+    this.checkpointer = checkpointer;
+  }
+
+  @Override
+  public void ack(Event event) {
+    if (acked.compareAndSet(false, true)) {
+      try {
+        checkpointer.checkpoint(
+            consumerRecord.sequenceNumber(), consumerRecord.subSequenceNumber());
+      } catch (Exception e) {
+        throw new MessageAcknowledgementException(
+            String.format(
+                "Failed to acknowledge record %s:%d",
+                consumerRecord.sequenceNumber(), consumerRecord.subSequenceNumber()),
+            e);
+      }
+      logger.atInfo().log(
+          "Checkpointed record %s:%d",
+          consumerRecord.sequenceNumber(), consumerRecord.subSequenceNumber());
+    }
+  }
+}
